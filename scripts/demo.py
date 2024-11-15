b7a552612327be9ed9cadea5b8ed57a8f4f9ae29
HaoliangWang <hlwang1997@gmail.com>
2024-11-15 19:01:13 +0000

    before meeting with xiaolin

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,14 +1,16 @@
 import os
 import glob
 import argparse
+from PIL import Image
+import numpy as np
 from uncos import UncOS
 from uncos.uncos_utils import load_data_npy
 
 
-def main(vis=True, most_likely=False):
+def main(path, vis=True, most_likely=False):
     test_most_likely = most_likely
 
-    demo_files_list = glob.glob(os.path.join(os.path.dirname(__file__), f"../demo_files/**.npy"))
+    demo_files_list = glob.glob(os.path.join(os.path.dirname(__file__), f"../haoliang/{path}/**.npy"))
     uncos = UncOS()
     for demo_file_path in demo_files_list:
         print(f"Testing {demo_file_path}")
@@ -21,11 +23,19 @@ def main(vis=True, most_likely=False):
         else:
             uncos.visualize_confident_uncertain(pred_masks_boolarray, uncertain_hypotheses,
                                                 show=False, save_path=f'{demo_file_path[:-4]}_result.png')
+            
+        # for i, mask in enumerate(pred_masks_boolarray):
+        #     mask_3d = np.stack((mask,mask,mask),axis=2) #3 channel mask
+        #     masked_im = rgb_im*mask_3d
+        #     # masked_im[mask==0] = 255 # Optional
+        #     im = Image.fromarray(masked_im)
+        #     im.save(f'{demo_file_path[:-4]}_masks_{i}.png')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', help='Path to npy files.')
     parser.add_argument('-v', '--vis', action='store_true', help='Visualize the segmentation result.')
     parser.add_argument('-m', '--mostlikely', action='store_true', help='Return most likely result.')
     args = parser.parse_args()
-    main(vis=args.vis, most_likely=args.mostlikely)
+    main(path=args.path, vis=args.vis, most_likely=args.mostlikely)
---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+scenarios="collide contain drop dominoes link roll support"
+# scenarios="dominoes"
+for val in $scenarios; do
+    tmux kill-session -t ${val}
+    tmux new-session -s ${val} -d
+    tmux send-keys -t ${val} "conda activate uncos" Enter 
+    tmux send-keys -t ${val} "export LD_LIBRARY_PATH=/opt/conda/envs/uncos/lib/python3.10/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH" Enter 
+    # tmux send-keys -t ${val} "python demo.py -p ${val}" Enter 
+    tmux send-keys -t ${val} "python extract.py --scenario ${val}" Enter 
+    echo $val
+done
