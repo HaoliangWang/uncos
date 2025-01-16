37b10c42af6dafd18ab482a005bffbc21080796f
HaoliangWang <hlwang1997@gmail.com>
2025-01-15 21:08:45 -0500

    misc

--- scripts/xiaolin.py
+++ scripts/xiaolin.py
@@ -8,14 +8,13 @@ from uncos.uncos_utils import load_data_npy
 def main(vis=True, most_likely=False):
     test_most_likely = most_likely
 
-    demo_files_list = glob.glob(os.path.join(os.path.dirname(__file__), f"/ccn2/u/haw027/b3d_ipe/uncos_results/problematic/contain/**.npy"))
+    demo_files_list = glob.glob(os.path.join(os.path.dirname(__file__), f"../demo_files/**.npy"))
     uncos = UncOS()
     for demo_file_path in demo_files_list:
         print(f"Testing {demo_file_path}")
         rgb_im, pcd = load_data_npy(demo_file_path)
         pred_masks_boolarray, uncertain_hypotheses = uncos.segment_scene(rgb_im, pcd,
                                                                          return_most_likely_only=test_most_likely,
-                                                                         pointcloud_frame='world',
                                                                          n_seg_hypotheses_trial=12)
         if vis:
             uncos.visualize_confident_uncertain(pred_masks_boolarray, uncertain_hypotheses)
